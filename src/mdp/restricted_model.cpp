2030318fc54e2dc01808073f0aa99ca76073353e
piyushk <piyushk@gmail.com>
2016-06-26 16:19:26 -0400

    started work on restricted model.

--- src/mdp/restricted_model.cpp
+++ src/mdp/restricted_model.cpp
@@ -1,36 +1,77 @@
 #include <utexas_guidance/mdp/restricted_model.h>
 
-/* The general ideas are:
- * get rid of the thin
+/*
+ * max assigned robots.
+ * max distance away
+ * structure (DIRECTS then leads RELEASES then ASSIGNS then LEADS then WAIT)
+ * Don't assign robot to location where it's been released from.
+ * ensure if robot is colocated, then lead or guide is called prior to wait.
+ * do not assign robot to a location where another robot is.
  *
+ * only one we can't do is releasing a robot after directing a human, as that one's a bit iffy and will require saving
+ * more information. add prev_action to guidance_model, and add heuristics to that instead of doing the separate class.
+ * It'll make life easier. just make sure assigning a single robot instead of having to choose a robot  does not conflate time and distance.
+ */
+
 namespace utexas_guidance {
 
-  std::string GuidanceModel::getName() const {
+  std::string RestricteModel::getName() const {
     return std::string("RestrictedModel");
   }
 
-  void RestrictedModel::getActionsAtState(const ExtendedState& state,
+  void RestrictedModel::getActionsAtState(const utexas_planning::State::ConstPtr& state_base,
                                           std::vector<Action>& actions) {
+
+    State::ConstPtr state = boost::dynamic_pointer_cast<const State>(state_base);
+    if (!state) {
+      throw utexas_planning::DowncastException("utexas_planning::State", "utexas_guidance::State");
+    }
+
     actions.clear();
     int action_counter = 0;
 
-    // Force releasing any robots that directed a person last time around.
+    // Wait is allowed at all times.
+    Action::Ptr action(new Action(WAIT, 0, 0));
+    actions.push_back(action);
+    ++action_counter;
 
-    // If the previous action was DIRECT_HUMAN, then this action is fixed.
-    if (state.prev_action.type == DIRECT_PERSON) {
-      actions.push_back(Action(RELEASE_ROBOT, state.prev_action.robot_id));
-      return;
+    State::ConstPtr state = boost::dynamic_pointer_cast<const State>(state_base);
+    if (!state) {
+      throw utexas_planning::DowncastException("utexas_planning::State", "utexas_guidance::State");
     }
 
-    // Check if WAIT is a possible action. If LEAD_PERSON has been called, then return WAIT as the only valid action.
-    int colocated_robot_id = getColocatedRobotId(state);
-    if (state.assist_type != NONE || colocated_robot_id == NONE) {
-      actions.push_back(Action(WAIT));
-      ++action_counter;
+    // Allow robots to be assigned/released at any given location.
+    for (unsigned int robot = 0; robot < state->robots.size(); ++robot) {
+      if (state->robots[robot].help_destination == NONE) {
+        actions.resize(action_counter + num_vertices_);
+        for (unsigned int node = 0; node < num_vertices_; ++node) {
+          actions[action_counter] = Action::Ptr(new Action(ASSIGN_ROBOT, robot, node));
+          ++action_counter;
+        }
+      } else if (!(state->robots[robot].is_leading_person)) {
+        actions.push_back(Action::Ptr(new Action(RELEASE_ROBOT, robot)));
+        ++action_counter;
+      }
     }
-    if (state.assist_type == LEAD_PERSON) {
-      return;
+
+    // Allow leading/guiding via all colocated robots.
+    std::vector<std::pair<int, int> > robot_request_ids;
+    getColocatedRobotRequestIds(*state, robot_request_ids);
+    for (unsigned int idx_num = 0; idx_num < robot_request_ids.size(); ++idx_num) {
+      int robot_id = robot_request_ids[idx_num].first;
+      /* const RobotState& robot = state->robots[robot_id]; */
+      int request_id = robot_request_ids[idx_num].second;
+      const RequestState& request = state->requests[request_id];
+      actions.resize(action_counter + 2 * adjacent_vertices_map_[request.loc_node].size());
+      for (unsigned int adj = 0; adj < adjacent_vertices_map_[request.loc_node].size(); ++adj) {
+        actions[action_counter] =
+          Action::Ptr(new Action(DIRECT_PERSON, robot_id, adjacent_vertices_map_[request.loc_node][adj], request_id));
+        actions[action_counter + 1] =
+          Action::Ptr(new Action(LEAD_PERSON, robot_id, adjacent_vertices_map_[request.loc_node][adj], request_id));
+        action_counter += 2;
+      }
     }
+    // If direct and lead are legal actions, remove other actions.
 
     // Check if leading a person is allowed here.
     if (colocated_robot_id != NONE) {
