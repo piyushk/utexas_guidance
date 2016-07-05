afc1765209ef12522c801079c0f28bec5832f286
Piyush Khandelwal <piyushk@gmail.com>
2016-07-05 00:47:25 -0500

    got visualizer to compile, but need to implement visualization functionality.

--- include/utexas_guidance/mdp/visualizer.h
+++ include/utexas_guidance/mdp/visualizer.h
@@ -1,14 +1,40 @@
 #ifndef UTEXAS_GUIDANCE_VISUALIZER_H
 #define UTEXAS_GUIDANCE_VISUALIZER_H
 
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include <boost/thread.hpp>
 #include <QGLViewer/qglviewer.h>
-#include <utexas_guidance/core/visualizer.h>
+#include <utexas_guidance/mdp/structures.h>
+#include <utexas_planning/core/visualizer.h>
+#include <qapplication.h>
 
 namespace utexas_guidance {
 
   class StateViewer : public QGLViewer {
 
+    public:
+
+      virtual ~StateViewer();
+
+      typedef boost::shared_ptr<StateViewer> Ptr;
+      typedef boost::shared_ptr<const StateViewer> ConstPtr;
+
+      virtual void startEpisode(const utexas_planning::State::ConstPtr& start_state);
+      virtual void updateState(const utexas_planning::State::ConstPtr& state, float timeout = 0.0f);
+
     protected:
+
+      virtual void drawState(const State::ConstPtr& state);
+      virtual void drawInterpolatedState(const State::ConstPtr& state1,
+                                         const State::ConstPtr& state2,
+                                         float ratio);
+
+      boost::mutex mutex_;
+      State::ConstPtr current_state_;
+      State::ConstPtr next_state_;
+      float time_to_next_state_;
+
+      boost::posix_time::ptime time_at_state_update_;
 
       virtual void init();
       virtual void draw();
@@ -23,14 +49,17 @@ namespace utexas_guidance {
 
       virtual ~Visualizer();
       virtual void init(int argc, char* argv[]);
-      virtual void startEpisode(const State::ConstPtr& start_state);
-      virtual void updateState(const State::ConstPtr& state, float timeout = 0.0f);
+      virtual void startEpisode(const utexas_planning::State::ConstPtr& start_state);
+      virtual void updateState(const utexas_planning::State::ConstPtr& state, float timeout = 0.0f);
+      virtual void exec();
 
     protected:
 
       StateViewer::Ptr viewer_;
+      boost::shared_ptr<QApplication> application_;
+
   };
-  
+
 } /* utexas_guidance */
 
 #endif /* end of include guard: UTEXAS_GUIDANCE_VISUALIZER_H */
--- src/mdp/visualizer.cpp
+++ src/mdp/visualizer.cpp
@@ -1,60 +1,96 @@
 #include <class_loader/class_loader.h>
-#include <utexas_planning/visualizers/no_visualization.h>
-#include <qapplication.h>
+#include <utexas_guidance/mdp/visualizer.h>
+#include <utexas_planning/common/exceptions.h>
 
 namespace utexas_guidance {
+
+  StateViewer::~StateViewer() {}
+
+  void StateViewer::startEpisode(const utexas_planning::State::ConstPtr& start_state) {
+    boost::mutex::scoped_lock lock(mutex_);
+    current_state_ = boost::dynamic_pointer_cast<const State>(start_state);
+    if (!current_state_) {
+      throw utexas_planning::DowncastException("utexas_planning::State", "utexas_guidance::State");
+    }
+    next_state_.reset();
+    time_at_state_update_ = boost::posix_time::microsec_clock::local_time();
+    time_to_next_state_ = 0.0;
+  }
+
+  void StateViewer::updateState(const utexas_planning::State::ConstPtr& state_base, float timeout) {
+    boost::mutex::scoped_lock lock(mutex_);
+    State::ConstPtr state = boost::dynamic_pointer_cast<const State>(state_base);
+    if (!state) {
+      throw utexas_planning::DowncastException("utexas_planning::State", "utexas_guidance::State");
+    }
+    if (next_state_) {
+      current_state_ = next_state_;
+    }
+    if (timeout == 0.0f) {
+      current_state_ = state;
+    } else {
+      next_state_ = state;
+    }
+    time_to_next_state_ = timeout;
+    time_at_state_update_ = boost::posix_time::microsec_clock::local_time();
+  }
+
+  void StateViewer::drawState(const State::ConstPtr& state) {
+
+
+  }
+
+  void StateViewer::drawInterpolatedState(const State::ConstPtr& state1,
+                                          const State::ConstPtr& state2,
+                                          float ratio) {
+
+
+  }
+
+  void StateViewer::init() {
+    boost::mutex::scoped_lock lock(mutex_);
+    glDisable(GL_LIGHTING);
+    glPointSize(10.0f);
+    glLineWidth(3.0f);
+    setGridIsDrawn(false);
+  }
+
+  void StateViewer::draw() {
+    boost::mutex::scoped_lock lock(mutex_);
+    boost::posix_time::ptime current_time = boost::posix_time::microsec_clock::local_time();
+    float time_since_state_update = (current_time - time_at_state_update_).total_milliseconds();
+    if (!next_state_) {
+      drawState(current_state_);
+    } else if (time_since_state_update > time_to_next_state_) {
+      current_state_ = next_state_;
+      next_state_.reset();
+      drawState(current_state_);
+    } else {
+      drawInterpolatedState(current_state_, next_state_, time_since_state_update / time_to_next_state_);
+    }
+  }
 
   Visualizer::~Visualizer() {}
 
   void Visualizer::init(int argc, char* argv[]) {
     QApplication application(argc, argv);
-    viewer_.reset(new Viewer);
-    viewer_->setWindowTitle("out");
+    viewer_.reset(new StateViewer);
+    viewer_->setWindowTitle("GuidanceVisualizer");
     viewer_->show();
   }
-  void Visualizer::startEpisode(const State::ConstPtr& /* start_state */) {}
-  void Visualizer::updateState(const State::ConstPtr& /* state */, float /* timeout */) {}
+
+  void Visualizer::startEpisode(const utexas_planning::State::ConstPtr& start_state) {
+    viewer_->startEpisode(start_state);
+  }
+
+  void Visualizer::updateState(const utexas_planning::State::ConstPtr& state, float timeout) {
+    viewer_->updateState(state, timeout);
+  }
+
+  void Visualizer::exec() {
+    application_->exec();
+  }
 
 } /* utexas_planning */
 
 CLASS_LOADER_REGISTER_CLASS(utexas_guidance::Visualizer, utexas_planning::Visualizer);
-
-#include <iostream>
-#include <utexas_guidance/graph/graph.h>
-
-utexas_guidance::Graph graph_;
-
-class GraphViewer : public QGLViewer {
-  protected :
-    virtual void draw();
-    virtual void init();
-};
-
-// Draws a spiral
-void GraphViewer::draw() {
-  utexas_guidance::draw(graph_);
-}
-
-void GraphViewer::init() {
-  // Restore previous viewer state.
-  restoreStateFromFile();
-
-  glDisable(GL_LIGHTING);
-  glPointSize(10.0f);
-  glLineWidth(3.0f);
-  setGridIsDrawn(false);
-}
-
-int main(int argc, char *argv[]) {
-  if (argc < 2 || argc > 2) {
-    std::cerr << "Usage: " << argv[0] << " <graph-file>" << std::endl;
-    return -1;
-  }
-
-  utexas_guidance::readGraphFromFile(std::string(argv[1]), graph_);
-
-
-
-  return application.exec();
-}
-
